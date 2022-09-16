de6762e65685705960cd3f1fe964d3da3066b860
Kevin Rocker <kevin.j.rocker@gmail.com>
2022-09-16 10:14:08 -0400

    Creating the Issue

--- packages/feature-lib/src/lib/feature-lib.tsx
+++ packages/feature-lib/src/lib/feature-lib.tsx
@@ -1,3 +1,4 @@
+import { BaseLib } from '@nx-test/base-lib';
 import styles from './feature-lib.module.css';
 
 /* eslint-disable-next-line */
@@ -7,6 +8,7 @@ export function FeatureLib(props: FeatureLibProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to FeatureLib!</h1>
+      <BaseLib />
     </div>
   );
 }
